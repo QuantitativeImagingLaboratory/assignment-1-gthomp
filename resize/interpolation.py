70ec9e2d92c4580c5f3033eabd5d6e2ba311a430
Greg Thompson <cbk1011.gt@gmail.com>
2017-10-07 02:28:46 -0500

    first commit

--- resize/interpolation.py
+++ resize/interpolation.py
@@ -10,19 +10,29 @@ class interpolation:
 
         #Write your code for linear interpolation here
 
-        return 0
+        unknown_intensity = pt1+(unknown*(pt2-pt1))
+        return unknown_intensity
+
 
     def bilinear_interpolation(self, pt1, pt2, pt3, pt4, unknown):
         """Computes the linear interpolation for the unknown values using pt1 and pt2
         take as input
-        pt1: known point pt1 and f(pt1) or intensity value
-        pt2: known point pt2 and f(pt2) or intensity value
-        pt1: known point pt3 and f(pt3) or intensity value
-        pt2: known point pt4 and f(pt4) or intensity value
+        pt1: known point pt1 and f(pt1) or intensity value NW
+        pt2: known point pt2 and f(pt2) or intensity value NE
+        pt1: known point pt3 and f(pt3) or intensity value SW
+        pt2: known point pt4 and f(pt4) or intensity value SE
         unknown: take and unknown location
         return the f(unknown) or intentity at unknown"""
 
         # Write your code for bilinear interpolation here
         # May be you can reuse or call linear interpolatio method to compute this task
 
-        return 0
+        i1 = self.linear_interpolation(pt1,pt2,unknown[0])
+        i2 = self.linear_interpolation(pt3,pt4,unknown[0])
+        i3 = self.linear_interpolation(i1,i2,unknown[1])
+        return i3
+
+        #or it could be
+        """"""
+
+
--- resize/resample.py
+++ resize/resample.py
@@ -1,6 +1,8 @@
+import cv2
+import math
 class resample:
 
-    def resize(self, image, fx = None, fy = None, interpolation = None):
+    def resize(self, image, scalex = None, scaley = None, interpolation = None):
         """calls the appropriate funciton to resample an image based on the interpolation method
         image: the image to be resampled
         fx: scale along x direction (eg. 0.5, 1.5, 2.5)
@@ -10,12 +12,12 @@ class resample:
         """
 
         if interpolation == 'bilinear':
-            return self.bilinear_interpolation(image, fx, fy)
+            return self.bilinear_interpolation(image, scalex, scaley)
 
         elif interpolation == 'nearest_neighbor':
-            return self.nearest_neighbor(image, fx, fy)
+            return self.nearest_neighbor(image, scalex, scaley)
 
-    def nearest_neighbor(self, image, fx, fy):
+    def nearest_neighbor(self, image, scalex, scaley):
         """resizes an image using bilinear interpolation approximation for resampling
         image: the image to be resampled
         fx: scale along x direction (eg. 0.5, 1.5, 2.5)
@@ -24,11 +26,13 @@ class resample:
         """
 
         #Write your code for nearest neighbor interpolation here
+        img = cv2.imread(image)
+        res = cv2.resize(img, fx=scalex, fy=scaley, interpolation=cv2.INTER_NEAREST)
 
-        return image
+        return res
 
 
-    def bilinear_interpolation(self, image, fx, fy):
+    def bilinear_interpolation(self, image, scalex, scaley):
         """resizes an image using bilinear interpolation approximation for resampling
         image: the image to be resampled
         fx: scale along x direction (eg. 0.5, 1.5, 2.5)
@@ -38,5 +42,21 @@ class resample:
 
         # Write your code for bilinear interpolation here
 
-        return image
+        (ow,oh) = image.shape # stores the old image width and height, BE SURE THAT OW AND OH ARE IN THE CORRECT ORDER!!
+        (nw,nh) = (math.floor(scalex*ow),math.floor(scaley*oh)) # new width and height,again check the (ow,oh) order
+        new_img = np.zeros(nw,nh) #create matrix populated with zeros,check (ow,oh)
+        for(i,j) in new_img:
+            ti = i/nw
+            tj = j/nh
+            old_i = math.floor(ti*ow) #the "old image" i coordinate
+            old_j = math.floor(tj*oh)
+            tl = image[old_i,old_j] # the top left coordinate
+            tr = image[old_i+1, old_j] # top right
+            bl = image[old_i, old_j+1] # bottom left coordinate THINK THIS MAY BE [OLD_I+1,OLD_J]
+            br = image[old_i+1, old_j+1] # bottom right
+            ui = ti*ow-old_i #may be ti*oh-old_j
+            uj = tj*oh-old_j #see ui comment
+            res = bilinear_interpolation(tl,tr,bl,br,(ui,uj))
+
+        return res
 
