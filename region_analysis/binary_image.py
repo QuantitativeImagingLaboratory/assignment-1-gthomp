9172adac4c61afbf108b3a74db807936e592c053
Greg Thompson <cbk1011.gt@gmail.com>
2017-10-07 17:15:59 -0500

    first commit for part 2

--- region_analysis/binary_image.py
+++ region_analysis/binary_image.py
@@ -8,10 +8,25 @@ class binary_image:
         image: a grey scale image
         returns a histogram"""
 
-        hist = [0]*256
+        (row, col) = image.shape
+        hist = [0]*256 # I think this line and the following loops is calculating the frequency of pixels
+
+        for i in range(row):
+            for j in range(col):
+                hist[image[i, j]] += 1
 
 
         return hist
+
+
+    def expected_value(self, pdf, offset, total):
+        exp = 0
+        for i, val in enumerate(pdf):
+            exp += (i+offset)*val/total
+        return exp
+
+
+
 
     def find_optimal_threshold(self, hist):
         """analyses a histogram it to find the optimal threshold value assuming a bimodal histogram
@@ -19,8 +34,19 @@ class binary_image:
         hist: a bimodal histogram
         returns: an optimal threshold value"""
 
-        threshold = 0
-
+        threshold = int(len(hist)/2)
+        num_pixels = sum(hist) #width*height
+        (uprev,vprev) = (0,0)
+        u = expected_value(hist[:threshold],0,num_pixels)
+        v = expected_value(hist[t:],threshold,num_pixels)
+        threshold = int((u+v)/2)
+        (du,dv) = (u-uprev,v-vprev)
+        while du != 0 and dv != 0:
+            (uprev, vprev) = (u, v)
+            u = expected_value(hist[:threshold], 0, num_pixels)
+            v = expected_value(hist[t:], threshold, num_pixels)
+            threshold = int((u + v) / 2)
+            (du, dv) = (u - uprev, v - vprev)
 
         return threshold
 
@@ -31,6 +57,15 @@ class binary_image:
         returns: a binary image"""
 
         bin_img = image.copy()
+        hist = compute_histogram(image)
+        t = find_optimal_threshold(hist)
+        for i in range(row):
+            for j in range(col):
+                if image[i,j] <= t:
+                    bin_img[i,j] = 0
+                else:
+                    bin_img[i,j] = 255
+
 
         return bin_img
 
--- region_analysis/cell_counting.py
+++ region_analysis/cell_counting.py
@@ -1,3 +1,6 @@
+from collections import Counter
+#import matplotlib.pyplot as plt #MAY NEED TO UNCOMMENT THIS
+#import numpy as np    MAY NOT NEED TO IMPORT THIS HERE
 class cell_counting:
 
     def blob_coloring(self, image):
@@ -6,15 +9,61 @@ class cell_counting:
         image: binary image
         return: a list of regions"""
 
-        regions = dict()
+        #regions = dict()
+        (row,col) = image.shape
+        regions = np.zeros(row,col,dtype = np.uint32) #make this uint32
+        region_dict = {}
+        k = 1
+        for i in range(row):
+            for j in range(col):
+                if image[i,j] == 1 and image[i,j-1] == 0 and image[i-1,j] == 0:
+                    regions[i,j] = k
+                    region_dict[k] = set([i,j]) # NEW   may not need to be [] around i,j
+                    k += 1
+                elif image[i,j] == 1 and image[i,j-1] == 0 and image[i-1,j] == 1: #bottom left in pic
+                    regions[i,j] = regions[i-1,j] #R[c] = R[n]
+                    region_dict[regions[i,j]].add(i,j) # for the k value in regions matrix(key) add the coordinate tuple to the list of values associated with the key
+                elif image[i,j] == 1 and image[i,j-1] == 1 and image[i-1,j] == 0:
+                    regions[i,j] = regions[i,j-1]
+                    region_dict[regions[i,j]].add(i,j) #top right in pic
+                elif image[i,j] == 1 and image[i,j-1] == 1 and image[i-1,j] == 1:
+                    regions[i, j] = regions[i - 1, j]  # R[c] = R[n]
+                    if regions[i-1,j] == regions[i,j-1]: # if the pixels are black AND the regions are the same
+                        region_dict[regions[i,j]].add(i,j)
+                        continue
+                    for r in region_dict[regions[i,j-1]]:
+                        regions[r[0],r[1]] = regions[i,j] #LOOK AGAIN
+                    union = region_dict[regions[[i-1,j]]] or region_dict[regions[i,j-1]]
+                    union.add(i,j)
+                    region_dict[regions[i,j-1]].clear() #SHOULD THIS BE DEL INSTEAD OF CLEAR??
+                    region_dict[regions[i,j]] = union
+                if regions[i,j-1] != regions[i-1,j]: # if pixels (left and top) are black and regions are different, make regions the same
+                    regions[i,j-1] = regions[i-1,j]
 
-        return regions
 
-    def compute_statistics(self, region):
-        """Compute cell statistics area and location
+
+        return region_dict
+
+    def compute_statistics(self, region_dict):
+        """Compute cell statistics area and location(centroid)
         takes as input
         region: a list of pixels in a region
         returns: area"""
+        min_area = 15
+        stats = {}
+        for k,pixels in region_dict.items():
+            area = len(pixels)
+            if area < min_area:
+                continue
+            (ci,cj) = (0,0)
+            for (i,j) in pixels:
+                ci += i
+                cj += j
+            ci /= area
+            cj /= area
+            stats[k] = {'x':cj,'y':ci, 'area':area}
+
+        print(stats)
 
 
 
@@ -30,6 +79,15 @@ class cell_counting:
         image: a list of pixels in a region
         stats: stats regarding location and area
         returns: image marked with center and area"""
+
+        plt.clf()
+        plt.imshow(image,interpolation='none',cmap=plt.cm.gray)
+        for k,region_stats in stats.items():
+            (x,y) = (region_stats['x'],region_stats['y'])
+            plt.plot(x,y,'r*') # plots red asterisk
+            plt.plot(x,y,"{}:{}".format(k,region_stats['area']))
+        plt.show()
+        plt.savefig('output/cellct/result.png')
 
         return image
 
